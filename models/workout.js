3fc15fd8c4f184a0c9105c3a70a509d00ea8fac3
Alex Fuss <alexfuss2@gmail.com>
2020-09-22 18:14:53 -0400

    Initial models created

---
+++ models/workout.js
@@ -0,0 +1,41 @@
+// Require mongoose
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Add schema for workout data
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [{
+    type: {
+      type: String,
+      trim: true,
+      required: 'exercise field cannot be blank.'
+    },
+    name: {
+      type: String,
+      required: 'enter a name for exercise.'
+    },
+    duration: {
+      type: Number
+    },
+    weight: {
+      type: Number
+    },
+    reps: {
+      type: Number
+    },
+    sets: {
+      type: Number
+    },
+    distance: {
+      type: Number
+    }
+  }]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
